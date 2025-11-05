840f98029ffc3e7749da89174bfe6a6a8f9b841e
hxjwb <newasdsd@163.com>
2025-11-05 23:50:20 +0800

    Revert "tooth by AI"

--- modules/rtp_rtcp/source/video_fec_generator.h
+++ modules/rtp_rtcp/source/video_fec_generator.h
@@ -32,7 +32,7 @@ class VideoFecGenerator {
   // Returns the overhead, in bytes per packet, for FEC (and possibly RED).
   virtual size_t MaxPacketOverhead() const = 0;
   // Current rate of FEC packets generated, including all RTP-level headers.
-  virtual DataRate CurrentFecRate() const { return DataRate::Zero(); }
+  virtual DataRate CurrentFecRate() const = 0;
   // Set FEC rates, max frames before FEC is sent, and type of FEC masks.
   virtual void SetProtectionParameters(
       const FecProtectionParams& delta_params,
