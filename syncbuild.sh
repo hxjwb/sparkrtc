b30faef588aeed41d40061bf0b82852fde4b6058
hxjwb <newasdsd@163.com>
2026-03-06 11:07:00 +0800

    chore: 提交当前改动

--- syncbuild.sh
+++ syncbuild.sh
@@ -1,5 +1,5 @@
 git add -A
 git commit -m "chore: 提交当前改动"
 git push
-ssh eez221 "cd sparkrtc && git pull"
-ssh eez221 "cd sparkrtc && ninja -C out/t"
+ssh eez221.ece.ust.hk "cd sparkrtc && git pull"
+ssh eez221.ece.ust.hk "cd sparkrtc && ninja -C out/t"
